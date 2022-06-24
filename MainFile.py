14fcdcf0799651f67c42d9fe0a34e5d1334af199
malkai <Omalkai14@gmail.com>
2022-06-24 08:45:54 -0300

    Refactoring project
    Refactoring Project and move unused files for deleting it later.

---
+++ MainFile.py
@@ -0,0 +1,20 @@
+import connect_firebase
+
+
+def calculate_carbon_emissions()
+    for i in range(n_leituras):
+        co2_m1 = calc_co2_speed(measures["RPM"][i],measures["INTAKE_PRESSURE"][i],measures["INTAKE_TEMP"][i])
+        print(co2_m1)
+
+
+    # fechar os graficos gerados pelo matplotlib para mostrar o quanto de CO2 foi consumido
+    print('====================================================================================================')
+
+    for i in range(n_leituras):
+        co2_m2 = calc_co2(measures["MAF"][i])
+        print(co2_m2)
+
+
+
+
+connect_firebase.getDoc()
