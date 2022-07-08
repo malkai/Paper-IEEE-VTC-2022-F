c3b9d31d3fdd9ec8d2c6b242a3f22cd686a2dbc2
malkai <Omalkai14@gmail.com>
2022-07-08 11:26:55 -0300

    Delete files

--- unused_file/vin.js
+++
@@ -1,8 +0,0 @@
-
-const vindec = require('vindec');
-var vinGenerator = require('vin-generator');
-
-var randomVin = vinGenerator.generateVin();
-//
-//console.log(`${vindec.validate('JM1BK32GX97ELPTE0')}`)
-console.log(JSON.stringify(vindec.decode('ZFFEW58A660144998'))) 
